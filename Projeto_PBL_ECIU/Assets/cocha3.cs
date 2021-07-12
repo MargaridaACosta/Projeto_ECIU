d80939c4534f5af2082ec88698eb75f7f07afd87
JuditeResende <juditemresende@ua.pt>
2021-07-12 17:20:57 +0100

    game1

---
+++ Projeto_PBL_ECIU/Assets/cocha3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cocha3 : MonoBehaviour
+{
+    public Transform cochasss;
+    public float AngularSpeed, RotationRadius;
+    public float posX, posY, posZ, angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX = cochasss.position.x + Mathf.Cos(angle) * RotationRadius;
+        posZ = cochasss.position.z + Mathf.Sin(angle) * RotationRadius;
+        transform.position = new Vector3(posX, posY, posZ);
+
+        angle = angle + Time.deltaTime * AngularSpeed;
+
+        if (angle >= 360)
+        {
+            angle = 0;
+        }
+    }
+}
