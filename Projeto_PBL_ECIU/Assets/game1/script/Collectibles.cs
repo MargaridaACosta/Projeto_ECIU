eca0d31d566528d0b1889a03bd72568ac491f49a
JuditeResende <juditemresende@ua.pt>
2021-07-13 18:41:37 +0100

    judite

---
+++ Projeto_PBL_ECIU/Assets/game1/script/Collectibles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Collectibles : MonoBehaviour
+{
+    private GameManager GameManager;
+    public int pointValue;
+    public AudioSource AudioSource;
+
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+        GameManager.UpdateScore(pointValue);
+        //Plays an Audio Source
+        AudioSource.Play();
+    }
+
+    void Update()
+    {
+       //if (GameManager.timeValue <= 0)
+        {
+            //GameManager.GameOver();
+        }
+    }
+
+}
