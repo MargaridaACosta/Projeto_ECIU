163e2110ef06819ace1bb7ea0817a7cb16942d3f
sara <47630784+saravilarinho@users.noreply.github.com>
2021-07-14 00:37:23 +0100

    game 2
    game 2

---
+++ Projeto_PBL_ECIU/Assets/game2/scripts/MainCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject back;
+
+   public void OnMouseDown ()
+    {
+        Debug.Log("done");
+
+        if (back.activeSelf && controller.canReveal)
+        {
+            back.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+
+    }
+
+    public void Unreveal()
+    {
+        back.SetActive(true);
+    }
+
+
+}
