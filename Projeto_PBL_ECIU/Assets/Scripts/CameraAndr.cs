a2676216773a3006525cb6534820c662389c3e13
MargaridaACosta <47631077+MargaridaACosta@users.noreply.github.com>
2021-07-16 11:17:36 +0100

    Inicio Camera

---
+++ Projeto_PBL_ECIU/Assets/Scripts/CameraAndr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAndr : MonoBehaviour
+{
+    static WebCamTexture backCamera;
+
+    void Start()
+    {
+        if (backCamera == null)
+        backCamera = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = backCamera;
+
+        if (!backCamera.isPlaying)
+        backCamera.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
