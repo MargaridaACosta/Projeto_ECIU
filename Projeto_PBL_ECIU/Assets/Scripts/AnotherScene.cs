c3b382bb93d8a22e24b7b6e12f4b9fa6011f772b
MargaridaACosta <47631077+MargaridaACosta@users.noreply.github.com>
2021-07-17 19:25:19 +0100

    map

---
+++ Projeto_PBL_ECIU/Assets/Scripts/AnotherScene.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.SceneManagement;
+
+public class AnotherScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         SceneManager.LoadScene (sceneName:"POPlacement");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
