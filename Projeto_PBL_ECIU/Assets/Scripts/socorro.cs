3f65c947e0e66b13eeeaad9b65966552750fe886
sara <47630784+saravilarinho@users.noreply.github.com>
2021-07-19 17:33:04 +0100

    as

---
+++ Projeto_PBL_ECIU/Assets/Scripts/socorro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class socorro : MonoBehaviour
+{
+
+    public void OnMouseDown()
+    {
+        Debug.Log("socoorrorroororor");
+    }
+
+}
