6ac7c7fe96c40dcf2d997490b504dd2a7b21f3a8
sara <47630784+saravilarinho@users.noreply.github.com>
2021-07-20 23:47:54 +0100

    tutorial visual

---
+++ Projeto_PBL_ECIU/Assets/Scripts/Tentativa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tentativa : MonoBehaviour
+{
+
+    [SerializeField] private GameObject SairTutorial;
+
+
+    public void OnMouseDown()
+    {
+        SairTutorial.SetActive(false);
+    }
+}
