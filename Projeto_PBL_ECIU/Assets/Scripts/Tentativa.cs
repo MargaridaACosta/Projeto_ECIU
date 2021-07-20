de6170bbad31faa1448544850d2e4c374dd02d28
sara <47630784+saravilarinho@users.noreply.github.com>
2021-07-21 00:39:20 +0100

    tutorial funcional

--- Projeto_PBL_ECIU/Assets/Scripts/Tentativa.cs
+++ Projeto_PBL_ECIU/Assets/Scripts/Tentativa.cs
@@ -6,11 +6,57 @@ using UnityEngine.SceneManagement;
 public class Tentativa : MonoBehaviour
 {
 
-    [SerializeField] private GameObject SairTutorial;
+    [SerializeField] private GameObject SairTutorial_1;
+    [SerializeField] private GameObject SairTutorial_2;
+    [SerializeField] private GameObject SairTutorial_3;
+
 
 
     public void OnMouseDown()
     {
-        SairTutorial.SetActive(false);
+      //  SairTutorial.SetActive(false);
     }
+
+
+    public void SairTutorial()
+    {
+        SairTutorial_1.SetActive(false);
+        SairTutorial_2.SetActive(false);
+        SairTutorial_3.SetActive(false);
+
+    }
+
+    public void AbrirTutorial()
+    {
+        SairTutorial_1.SetActive(true);
+        SairTutorial_2.SetActive(false);
+        SairTutorial_3.SetActive(false);
+    }
+
+    public void Avancar_2()
+    {
+        SairTutorial_1.SetActive(false);
+        SairTutorial_2.SetActive(true);
+    }
+
+    public void Avancar_3()
+    {
+        SairTutorial_2.SetActive(false);
+        SairTutorial_3.SetActive(true);
+    }
+
+    public void Voltar_p_1()
+    {
+        SairTutorial_1.SetActive(true);
+        SairTutorial_2.SetActive(false);
+    }
+
+
+    public void Voltar_p_2()
+    {
+        SairTutorial_2.SetActive(true);
+        SairTutorial_3.SetActive(false);
+    }
+
+    
 }
