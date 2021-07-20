21e215bc365f95737f564c7fd1d3d2b40ca1c4f3
sara <47630784+saravilarinho@users.noreply.github.com>
2021-07-20 18:36:28 +0100

    botao a funcionar

--- Projeto_PBL_ECIU/Assets/Scripts/socorro.cs
+++ Projeto_PBL_ECIU/Assets/Scripts/socorro.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class socorro : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class socorro : MonoBehaviour
     public void Hello()
     {
         Debug.Log("socoorrorroororor");
+    }
+
+
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene("MyPassport");
     }
 
 }
